b1e07f660cd872760132d4095c7b3be4b3ff6452
MariaGalecka <galecm5715195_aehit@students.vizja.pl>
2024-06-01 09:52:48 +0200

    1 commit

--- src/main/resources/com/example/pplab7/Main.java
+++ src/main/resources/com/example/pplab7/Main.java
@@ -1,13 +1,9 @@
-package src.main.resources.com.example.pplab7;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.stage.DirectoryChooser;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.stage.*;
 import java.io.File;
 
 public class Main extends Application {
@@ -17,39 +13,33 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-
         primaryStage.setTitle("File Browser and Search");
 
         directoryPathField = new TextField();
         directoryPathField.setPromptText("Enter directory path");
-
         searchField = new TextField();
         searchField.setPromptText("Enter search phrase");
 
         Button browseButton = new Button("Browse");
-        browseButton.setOnAction(e -> browseDirectory());
-
+        browseButton.setOnAction(event -> browseDirectory());
         Button searchButton = new Button("Search");
 
-        HBox hBox = new HBox(10, directoryPathField, browseButton);
-
+        HBox hBox = new HBox(directoryPathField, browseButton);
         VBox vBox = new VBox(10, hBox, searchField, searchButton);
 
         Scene scene = new Scene(vBox, 600, 200);
-        primaryStage.setScene(scene);
 
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     private void browseDirectory() {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         File selectedDirectory = directoryChooser.showDialog(null);
-
         if (selectedDirectory != null) {
             directoryPathField.setText(selectedDirectory.getAbsolutePath());
         }
     }
-
 
     public static void main(String[] args) {
         launch(args);
